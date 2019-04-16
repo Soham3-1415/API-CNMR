266999a021ee92ee54470b45c5a2f7bdd46f6a52
Soham Roy (keybase.io/sohamroy) <soham3.1415@gmail.com>
2019-04-16 15:02:13 -0500

    Use promises to solve molfiles Doesn't properly parse jcamp yet

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
-const request = require("request");
-const app = require("express")();
-
+const got = require("got");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = parseInt(process.env.PORT,10) || 3000;
+app.use(bodyParser.json());
 app.post('/mol2DInput', (req, res) => {
-	let mol = req.params.mol;
-
-	/*request.get('https://www.chemspider.com/InChI.asmx/MolToInChIKey', (error, response, body) => {
-		let json = JSON.parse(body);
-		console.log(body);
+	let mol = req.body.mol;
+	let newMol = "";/*got.post({
+		url: 'https://www.chemspider.com/InChI.asmx/MolToInChIKey',
+		form: {mol:mol}
+	}, (error, response, body) => {
+		let inChIKey = body.match(/[A-Z]{14}-[A-Z]{10}-[A-Z]/g)[0];
+		got.get(`https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchikey/${inChIKey}/SDF?record_type=3d`, function(err, res, body) {
+			return body;
+		});
+	});*/
+	let molPromise = got.post('https://www.chemspider.com/InChI.asmx/MolToInChIKey',
+		{
+			headers:{"Content-Type":"application/x-www-form-urlencoded"},
+			body:`mol=${mol}`
+		}
+		).then(res => {
+			let inChIKey = res.body.match(/[A-Z]{14}-[A-Z]{10}-[A-Z]/g)[0];
+			return got.get(`https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchikey/${inChIKey}/SDF?record_type=3d`
+			).then(res => {
+				return res.body;
+			}).catch(error => {console.log(error.response.body);return ""});
+		}).catch(error =>  {console.log(error.response.body);return "";});
+	let jcampPromise = got.post('https://script.epfl.ch/script/Service/20140827/6klsXZb3NS',
+		{
+			headers:{"Content-Type":"application/x-www-form-urlencoded"},
+			body:`molfile=${mol}`
+		}
+		).then(res => {
+			return res.body;
+		}).catch(error => {console.log(error.response.body);return "";});
+	Promise.all([molPromise,jcampPromise]).then(result => {
+		res.status(200).send({
+			mol:result[0],
+			jcamp:result[1]
+		});
 	});
-
-	const inChIKey = 'https://www.chemspider.com/InChI.asmx/MolToInChIKey' (method:'POST',body:`mol=${mol}`);*/
-	res.status(200).send({
-		req:mol,
-		mol:"",
-		jcamp:""
-	})
 });
-
-const PORT = 3000;
-   
 app.listen(PORT, () => {
 	console.log(`server running on port ${PORT}`)
 });
-
-//const inChIKey = 'https://www.chemspider.com/InChI.asmx/MolToInChIKey' (method:'POST',body:`mol=${mol}`);
-//const mol3D = `https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchikey/${inChIKey}/SDF?record_type=3d`;
-//const jcampJSON = 'https://script.epfl.ch/script/Service/20140827/6klsXZb3NS' (method:'POST',body:`molfile=${mol}`);
